fd965345acfe83513aa17d48ab216c2328cb1b69
fiki2002 <adepitanoluwatosin202@gmail.com>
2022-05-30 13:19:08 +0100

    Redesigned the splash screen and forgot password

--- lib/SignupProcess/splash_screen.dart
+++ lib/SignupProcess/splash_screen.dart
@@ -1,68 +1,24 @@
-import 'package:animated_text_kit/animated_text_kit.dart';
-import 'package:cryptowatch/OtherScreens/BottomNavigationScreen.dart';
-import 'package:cryptowatch/OtherScreens/testing_screen.dart';
-
-import 'package:cryptowatch/SignupProcess/OnboardingScreen.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:cryptowatch/SignupProcess/sign_up_screen.dart';
 import 'package:cryptowatch/constants.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+
 import 'package:page_transition/page_transition.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
-class SplashScreen extends StatefulWidget {
-  @override
-  State<SplashScreen> createState() => _SplashScreenState();
-}
-
-class _SplashScreenState extends State<SplashScreen> {
-  @override
-  void initState() {
-    // TODO: implement initState
-    _navigateToHome();
- 
-    super.initState();
-  }
-
-  _navigateToHome() async {
-    await Future.delayed(Duration(milliseconds: 7000), () {});
-
-    Navigator.of(context).pushReplacement(PageTransition(
-      child: 
-      //TestingScreen(),
-      SignUpScreen(),
-      type: PageTransitionType.fade,
-      duration: Duration(milliseconds: 1500),
-      curve: Curves.fastLinearToSlowEaseIn,
-    ));
-  }
-
-
-
-
-
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Center(
-        child: DefaultTextStyle(
-          style: GoogleFonts.poppins(
-              color: PrimaryDeepBlue,
-              fontWeight: FontWeight.w700,
-              fontSize: 36),
-          child: AnimatedTextKit(
-            animatedTexts: [
-              TypewriterAnimatedText('Cryptowatch',
-                  speed: Duration(milliseconds: 600),
-                  cursor: '',
-                  curve: Curves.fastLinearToSlowEaseIn),
-            ],
-          ),
-        ),
-
-        //Text('Cryptowatch', style: GoogleFonts.poppins( color: PrimaryDeepBlue, fontWeight: FontWeight.w700, fontSize: 36))
-      ),
+    return AnimatedSplashScreen(
+      splash: SvgPicture.asset('assets/images/cryptowatch_logo_white.svg'),
+      backgroundColor: Primary1,
+      nextScreen: SignUpScreen(),
+      duration: 1000 ,
+      splashTransition: SplashTransition.scaleTransition,
+      pageTransitionType: PageTransitionType.fade,
+      animationDuration: Duration(seconds: 2),
     );
   }
 }
